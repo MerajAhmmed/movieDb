0504e03e3d1cf82607c065e54b934cce7ae3a21f
MerajAhmmed <merajahmmed6@gmail.com>
2024-12-24 19:49:47 +0600

    last

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    LOCAL_BASE_URL: isProd
-      ? "movie-db-beryl-seven.vercel.app"
-      : "http://localhost:3000",
-  },
   images: {
     remotePatterns: [
       {
